7ea2430cc5c8f26c0ba3ed1c2fb0d6633d95d7a8
Timok Alekseev <timofey.alekseev@list.ru>
2025-04-04 10:00:11 +0300

    1.0 Release

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,24 @@
 #include <Arduino.h>
 #include <SoftwareSerial.h>
 
-#define BTN 11
-#define LED1 8
-#define LED2 7
+#define BTN 8
+#define LED1 3
+#define LED2 5
 #define LED3 6
-#define LED4 5
-#define LED5 4
-#define LED6 9
-#define TX 2
-#define RX 1
+#define LED4 9
+#define LED5 10
+#define LED6 11
+#define TX 12
+#define RX 13
 
 bool btnFlag = false;
 
 String val;
 
-SoftwareSerial myserial(2, 3);
+SoftwareSerial myserial(TX, RX);
 
 void setup()
 {
-  Serial.println(9600);
   pinMode(LED1, OUTPUT);
   pinMode(LED2, OUTPUT);
   pinMode(LED3, OUTPUT);
@@ -28,6 +27,14 @@ void setup()
   pinMode(LED6, OUTPUT);
   pinMode(BTN, INPUT);
 
+  digitalWrite(LED1, LOW);
+  digitalWrite(LED2, LOW);
+  digitalWrite(LED3, LOW);
+  digitalWrite(LED4, LOW);
+  digitalWrite(LED5, LOW);
+  digitalWrite(LED6, LOW);
+
+  myserial.begin(9600);
   myserial.println("Hi I am a SoftwareSerial, not Timofey!");
   randomSeed(analogRead(0));
 }
@@ -37,7 +44,7 @@ void loop()
   if (myserial.available())
   {
 
-    val = myserial.read();
+    val = myserial.readString();
 
     if (val == "MODE_1")
     {
@@ -47,10 +54,11 @@ void loop()
       digitalWrite(LED4, HIGH);
       digitalWrite(LED5, HIGH);
       digitalWrite(LED6, HIGH);
+      myserial.println("Mode1!");
     }
     else if (val == "MODE_2")
     {
-      do
+      while (!myserial.available())
       {
         digitalWrite(LED1, HIGH);
         digitalWrite(LED2, LOW);
@@ -59,41 +67,46 @@ void loop()
         digitalWrite(LED5, HIGH);
         digitalWrite(LED6, LOW);
         delay(500);
+        myserial.print("switching");
         digitalWrite(LED1, LOW);
         digitalWrite(LED2, HIGH);
         digitalWrite(LED3, LOW);
         digitalWrite(LED4, HIGH);
         digitalWrite(LED5, LOW);
         digitalWrite(LED6, HIGH);
-
-      } while (val == "MODE_1");
+        delay(500);
+      }
     }
     else if (val == "MODE_3")
     {
-      for (int i = 0; i < 255; i++)
+      while (!myserial.available())
       {
-        analogWrite(LED1, i);
-        analogWrite(LED2, i);
-        analogWrite(LED3, i);
-        analogWrite(LED4, i);
-        analogWrite(LED5, i);
-        analogWrite(LED6, i);
-        delay(10);
-      }
-      for (int i = 255; i > 0; i--)
-      {
-        analogWrite(LED1, i);
-        analogWrite(LED2, i);
-        analogWrite(LED3, i);
-        analogWrite(LED4, i);
-        analogWrite(LED5, i);
-        analogWrite(LED6, i);
-        delay(10);
+        for (int i = 0; i < 255; i++)
+        {
+          analogWrite(LED1, i);
+          analogWrite(LED2, i);
+          analogWrite(LED3, i);
+          analogWrite(LED4, i);
+          analogWrite(LED5, i);
+          analogWrite(LED6, i);
+          delay(10);
+        }
+
+        for (int i = 255; i > 0; i--)
+        {
+          analogWrite(LED1, i);
+          analogWrite(LED2, i);
+          analogWrite(LED3, i);
+          analogWrite(LED4, i);
+          analogWrite(LED5, i);
+          analogWrite(LED6, i);
+          delay(10);
+        }
       }
     }
     else if (val == "MODE_CHAOS")
     {
-      do
+      while (!myserial.available())
       {
         int randNumber = random(0, 255);
         analogWrite(LED1, randNumber);
@@ -102,18 +115,23 @@ void loop()
         analogWrite(LED4, randNumber);
         analogWrite(LED5, randNumber);
         analogWrite(LED6, randNumber);
-        delay(50);
-      } while (val == "MODE_CHAOS");
+        delay(5);
+      }
     }
-    else if (digitalRead(LED1))
+  }
+  if (digitalRead(BTN))
+  {
+    digitalWrite(LED1, LOW);
+    digitalWrite(LED2, LOW);
+    digitalWrite(LED3, LOW);
+    digitalWrite(LED4, LOW);
+    digitalWrite(LED5, LOW);
+    digitalWrite(LED6, LOW);
+    delay(5);
+    while (digitalRead(BTN))
     {
-      digitalWrite(LED1, LOW);
-      digitalWrite(LED2, LOW);
-      digitalWrite(LED3, LOW);
-      digitalWrite(LED4, LOW);
-      digitalWrite(LED5, LOW);
-      digitalWrite(LED6, LOW);
-      myserial.println("Shutting down...");
     }
+
+    myserial.println("Shutting down...");
   }
 }
